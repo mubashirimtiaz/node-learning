47e10c4906ae299b3afa0f746a829acdf1173f1b
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-11-08 21:35:41 +0500

    pushing aync code to remote branch

--- index.js
+++ index.js
@@ -1,121 +1,134 @@
-//FUNCTION DECLARATION
+// console.log('One');
 
-// function hello() {
-//   return 'Hello, World';
-// }
+// const getDataFromServer = (cb) => {
+//   console.log('getting data from server...');
 
-// const value = hello();
-
-//ANONYMOUS FUNCTION
-
-// let hello2 = function () {
-//   return 'hello again';
+//   setTimeout(() => {
+//     console.log('data received from server.');
+//     let data = '555';
+//     cb(data);
+//   }, 1000);
 // };
 
-// const returnedValue = hello2();
-// console.log(hello2);
-// console.log(returnedValue);
+// const callback = (data) => {
+//   console.log(data);
+// };
+// getDataFromServer();
 
-// function add(a = 5, b) {
-//   console.log(b);
-//   return a + b;
-// }
-
-// const resultOne = add();
-
-// console.log(resultOne);
-// const resultTwo = add();
-
-// console.log(resultOne);
-// console.log(resultTwo);
-
-//ARROW FUNCTION
-
-// const subtract = (a, b) => a - b;
-
-// const result = subtract(3, 5);
-// console.log(result);
-
-// const greet = (name) => console.log('Hello ' + name);
-
-// const result = greet('Okasha');
-// console.log(result);
-
-const arr = [1, 2, 3, 4];
-
-// const arr2 = arr;
-
-// arr2[2] = 0;
-
-//SPREAD OPERATOR
-
-// const arr3 = [...arr];
-
-// arr3[0] = 10;
-
-// console.log('arr1: ', arr);
-// console.log('arr2: ', arr2);
-
-// console.log('arr3: ', arr3);
-
-// const arr4 = [];
-
-// for (let i = 0; i > arr.length; i++) {
-//   arr4[i] = arr[i];
-// }
-
-// console.log(arr4);
-
-// const addition = (...params) => {
+// const doneWithAlluParatha = (message) => {
+//   console.log(message);
 // };
 
-// const result = addition(1, 2, 3, 4, 5);
-// console.log(result);
-
-//IIFE (IMMEDIATELY INVOKED FUNCTION EXPRESSION)
-
-// (function greet() {
-//   console.log('Hello World');
-// })();
-
-// RECURSIVE FUNCTION
-
-// function doSomething(num) {
-//   console.log('num in this iteration is: ' + num);
-//   if (num > 0) {
-//     doSomething(--num);
-//   }
-// }
-
-// doSomething();
-
-// const greet = (name) => {
-//   console.log(`Hello ${name}`);
+// const makeAlluParatha = (callback) => {
+//   setTimeout(() => {
+//     callback();
+//   }, 2000);
 // };
 
-// CLOSURES
-
-// const parent = () => {
-//   let num = 5;
-//   const child = () => {
-//     return 200 - num;
-//   };
-//   return child;
+// const makeMeethiLassi = (callback) => {
+//   setTimeout(() => {
+//     callback();
+//   }, 2000);
 // };
 
-// let result = parent()();
-// console.log(result);
+// makeAlluParatha(function () {
+//   console.log('Paratha ready to eat!');
+//   console.log('dakaaarr!!!');
+//   console.log('Want Lassi');
+//   makeMeethiLassi(function () {
+//     console.log('Lassi is ready!');
+//   });
+// });
 
-//CALLBACK
+function getAlluParathaPromise() {
+  const alluParathaPromise = new Promise((resolveCallback, rejectCallback) => {
+    const mood = 6;
+    setTimeout(() => {
+      if (mood > 5) {
+        resolveCallback('Paratha is ready!');
+      } else {
+        rejectCallback('Too tired');
+      }
+    }, 3000);
+  });
 
-// const sayHello = (greeting, name) => {
-//   greeting(name);
-// };
+  return alluParathaPromise;
+}
 
-// sayHello(greet, 'Ali');
+function main() {
+  getAlluParathaPromise()
+    .then((response) => {
+      console.log('response ', response);
+    })
+    .catch((error) => {
+      console.log('error ', error);
+    });
+}
 
-// const greet = () => {
-//   console.log('Hello Ali');
-// };
+// main();
 
-// setTimeout(greet, 3000);
+async function main2() {
+  try {
+    const result = await getAlluParathaPromise();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// main2();
+
+async function main3() {
+  try {
+    const result = await getAlluParathaPromise();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log('It will always run');
+  }
+}
+
+// main3();
+
+function getMeethiLassiPromise() {
+  const meethiLassiPromise = new Promise((resolveCallback, rejectCallback) => {
+    const stomachPosition = 7;
+    setTimeout(() => {
+      if (stomachPosition < 9) {
+        resolveCallback('Lassi is ready!');
+      } else {
+        rejectCallback("I'm full");
+      }
+    }, 3000);
+  });
+
+  return meethiLassiPromise;
+}
+
+async function main4() {
+  try {
+    const paratha = await getAlluParathaPromise();
+    const lassi = await getMeethiLassiPromise();
+    console.log('paratha ', paratha);
+    console.log('lassi ', lassi);
+  } catch (error) {
+    console.log('error ', error);
+  }
+}
+
+main4();
+
+function main5() {
+  getAlluParathaPromise()
+    .then((response) => {
+      console.log('response ', response);
+      return getMeethiLassiPromise();
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log('error ', error);
+    });
+}
